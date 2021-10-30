902d01dcb43cbe59443f16bce250fc212c9f1bce
poori90 <93428808+poori90@users.noreply.github.com>
2021-10-30 15:46:09 +0530

    Add files via upload

---
+++ MyFirstDayJavaProgram.java
@@ -0,0 +1,24 @@
+
+public class MyFirstDayJavaProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*int n=21;
+		if(n%3==0 && n%5==0 )
+		{
+			System.out.println("TRIZZ - FIZZ");
+		}
+		else if(n%5==0)
+		{
+			System.out.println("FIZZ");
+	}
+		else													
+			System.out.println("TRIZZ"); */
+		for (int i = 0; i <=50; i++) {
+			if(i%2==0)
+			{
+			System.out.println("Even Numbers" + i);
+		}
+}
+	}
+}
